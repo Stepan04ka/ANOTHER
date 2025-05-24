0a6ede1ea6da9084447025c37aff12b35a5bd8f2
Stepan04ka <155736993+Stepan04ka@users.noreply.github.com>
2025-05-24 19:27:10 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const Button = querySelector("button");
+
+Button.addEventListener("click", function() {
+    window.alert("You:ve clicked the button");
+});
+
+export function Test() {
+    console.log("I'm tryna to fix something");
+};
